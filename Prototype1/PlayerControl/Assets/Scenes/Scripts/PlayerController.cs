db4f3c67d90654eaf978547eefa36711f70350bc
Max <suffnessm@roverkids.org>
2025-11-24 14:12:51 -0500

    Finsish 1.4

--- Prototype1/PlayerControl/Assets/Scenes/Scripts/PlayerController.cs
+++ Prototype1/PlayerControl/Assets/Scenes/Scripts/PlayerController.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
 
-
-   public float speed = 5.0f;
+    // Private Variables
+   private float speed = 5.0f;
+   private float turnSpeed =25.0f;
+   private float horizontalInput;
+    private float forwardInput;
+    
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +19,13 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         //We'll move the vehicle foward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle  
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
