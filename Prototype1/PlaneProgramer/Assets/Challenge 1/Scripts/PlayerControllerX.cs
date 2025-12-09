bf131297bf1770326a4fa6930b17ab10451cdff5
Max <suffnessm@roverkids.org>
2025-12-09 13:17:41 -0500

    Finished plane programming

---
+++ Prototype1/PlaneProgramer/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 15f;
+    private float rotationSpeed = 50f;
+    private float horizontalInput;
+    private float verticalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * verticalInput);
+    }
+}
